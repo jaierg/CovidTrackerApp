f8520e637ea441d0fae8888272327bb8fa7b4dfe
Jaier Gordon <jaiergordon@jays-mbp.attlocal.net>
2020-04-28 18:55:49 -0400

    Updated the UI

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends React.Component{
     render(){
         return(
             <div>
-                <h1>App</h1>   
+                <h1>App Dev</h1>   
             </div>
         )
     }
